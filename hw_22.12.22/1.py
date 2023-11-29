2fd5b138f6d6fabcbbcc7c4c33d6435e4fae1d5e
VALERIK <valeria.lazkova26@gmail.com>
2023-11-29 03:07:21 +0300

    Update 1.py

--- hw_22.12.22/1.py
+++ hw_22.12.22/1.py
@@ -1,4 +1,6 @@
- def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+from typing import List
+
+def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         counter = []
         for city1, city2 in costs:
             counter.append([city2 - city1,city1,city2])
